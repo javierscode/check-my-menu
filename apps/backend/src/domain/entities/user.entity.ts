3c94359d11d6b4f12fe6691e9966cccbb0183def
javierscode <javihp79@gmail.com>
2022-11-18 17:17:02 +0100

    feat(backend): user get profile usecase

--- apps/backend/src/domain/entities/user.entity.ts
+++ apps/backend/src/domain/entities/user.entity.ts
@@ -2,6 +2,7 @@ import { EmailVO } from '@domain/value-objects/email.vo'
 import { NameVO } from '@domain/value-objects/name.vo'
 import { PasswordVO } from '@domain/value-objects/password.vo'
 import { UuidVO } from '@domain/value-objects/uuid.vo'
+import { Primitives } from 'src/types/primitives'
 
 export class User {
   constructor(
@@ -11,4 +12,14 @@ export class User {
     public email: EmailVO,
     public password: PasswordVO
   ) {}
+
+  public toPrimitives(): Primitives<User> {
+    return {
+      id: this.id.value,
+      name: this.name.value,
+      lastname: this.lastname.value,
+      email: this.email.value,
+      password: this.password.value,
+    }
+  }
 }
