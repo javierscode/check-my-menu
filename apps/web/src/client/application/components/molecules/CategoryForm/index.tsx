98ff21a8d7d205d09cd8c85c59fb83bfb374b761
javierscode <javihp79@gmail.com>
2022-12-21 16:23:04 +0100

    feat(web): add hook use-category-form

--- apps/web/src/client/application/components/molecules/CategoryForm/index.tsx
+++ apps/web/src/client/application/components/molecules/CategoryForm/index.tsx
@@ -1,22 +1,10 @@
-import { createCategory } from '@client/infrastructure/api/category/create-category.fetch'
-import { editCategory } from '@client/infrastructure/api/category/edit-category.fetch'
-import { uploadImage } from '@client/infrastructure/api/upload-image.fetch'
-import { yupResolver } from '@hookform/resolvers/yup'
+import { useCategoryForm } from '@client/infrastructure/hooks/use-category-form'
 import { Category } from '@shared/domain/entities/category'
 import Image from 'next/image'
-import { useRouter } from 'next/router'
-import { SubmitHandler, useForm } from 'react-hook-form'
 
 import { Input } from '../../atoms/Input'
 import { TextArea } from '../../atoms/TextArea'
 import styles from './CategoryForm.module.css'
-import { CategorySchema } from './schema'
-
-type Inputs = {
-  name: string
-  description: string
-  image: FileList
-}
 
 type Props = {
   item?: Category
@@ -24,48 +12,10 @@ type Props = {
 }
 
 export function CategoryForm({ item: category, onCloseForm }: Props) {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<Inputs>({
-    defaultValues: {
-      name: category?.name,
-      description: category?.description,
-    },
-    resolver: yupResolver(CategorySchema),
-  })
-  const router = useRouter()
-  const restaurantId = router.query.restaurantId as string
-
-  const editingMode = !!category
-
-  const onSubmit: SubmitHandler<Inputs> = async ({ name, description, image: ImageFile }) => {
-    let image: string | undefined
-    if (ImageFile.length > 0) {
-      image = await uploadImage(ImageFile[0], 'category')
-      if (!image) return
-    }
-
-    let newCategory: Category | undefined
-    if (editingMode) {
-      newCategory = await editCategory({
-        id: category?.id,
-        name,
-        description,
-        image: image || category?.image,
-        restaurantId,
-      })
-    } else {
-      if (!image) return
-      newCategory = await createCategory({ name, description, image, restaurantId })
-    }
-
-    onCloseForm?.(newCategory)
-  }
+  const { handleSubmit, editingMode, register, errors } = useCategoryForm(category, onCloseForm)
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <h1 className={styles.title}>{editingMode ? 'Edit' : 'Create'} Category</h1>
       <Input
         id='name'
--- apps/web/src/client/application/components/molecules/CategoryForm/schema.ts
+++
@@ -1,6 +0,0 @@
-import * as yup from 'yup'
-
-export const CategorySchema = yup.object().shape({
-  name: yup.string().required(),
-  description: yup.string().required(),
-})
