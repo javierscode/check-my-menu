de4890cd23a46814fd0822bc61c08ea468f850de
javierscode <javihp79@gmail.com>
2022-12-23 15:14:10 +0100

    fix(backend): add port env variable

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 // eslint-disable-next-line import/first
 import app from './app'
-const PORT = 3001
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`)
