32a40e4f0740dced67a292e2653f818ea0f9be0f
javierscode <javihp79@gmail.com>
2022-12-29 14:02:38 +0100

    feat(web): add use-login-form hook

---
+++ apps/web/src/client/infrastructure/hooks/use-login-form.ts
@@ -0,0 +1,55 @@
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import * as yup from 'yup'
+
+import { login } from '../api/user/login.fetch'
+import { useAuthContext } from '../contexts/auth.context'
+
+type Inputs = {
+  email: string
+  password: string
+}
+
+const LoginSchema = yup
+  .object({
+    email: yup.string().email().required(),
+    password: yup.string().required(),
+  })
+  .required()
+
+export function useLoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({
+    resolver: yupResolver(LoginSchema),
+  })
+  const router = useRouter()
+  const { updateAuth } = useAuthContext()
+  const [loginError, setLoginError] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const onSubmit: SubmitHandler<Inputs> = ({ email, password }) =>
+    login({ email, password, updateAuth, setError: setLoginError, router })
+
+  const onSubmitWithLoading = async (data: Inputs) => {
+    setIsLoading(true)
+    try {
+      await onSubmit(data)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+  return {
+    register,
+    handleSubmit: handleSubmit(onSubmitWithLoading),
+    errors,
+    loginError,
+    isLoading,
+  }
+}
